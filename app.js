769a274f74110d26b069d97212fe48955f93c22e
Stefan Focht <92666567+Sergfio@users.noreply.github.com>
2022-02-07 22:08:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,3 @@ document.addEventListener("click", () => {
     soundeffect.cloneNode().play();
 });
 
-let soundeffecte = new Audio("/sound/forcat.mp3");
-document.addEventListener("click", () => {
-    soundeffect.cloneNode().play();
-});
