0066fbb06114efad9fde994428edc706da95eaba
Stefan Focht <92666567+Sergfio@users.noreply.github.com>
2022-02-07 06:58:19 +0100

    stuck
    Beim Klicken soll ein Soundeffekt erzeugt werden

---
+++ app.js
@@ -0,0 +1,9 @@
+let soundeffect = new Audio("/sound/maus.mp3");
+document.addEventListener("click", () => {
+    soundeffect.cloneNode().play();
+});
+
+let soundeffecte = new Audio("/sound/forcat.mp3");
+document.addEventListener("click", () => {
+    soundeffect.cloneNode().play();
+});
