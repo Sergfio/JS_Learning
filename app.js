570b9bf9c83931827f93e47659f28f2507c8c04e
Stefan Focht <92666567+Sergfio@users.noreply.github.com>
2022-02-07 22:11:06 +0100

    Delete app.js
    fail

--- app.js
+++
@@ -1,9 +0,0 @@
-let soundeffect = new Audio("/sound/maus.mp3");
-document.addEventListener("click", () => {
-    soundeffect.cloneNode().play();
-});
-
-let soundeffecte = new Audio("/sound/forcat.mp3");
-document.addEventListener("click", () => {
-    soundeffect.cloneNode().play();
-});
